d3699f6d13507ee027edb5fae386beb5aa6d2cfc
Lucas Brito <lucasm_brito@hotmail.com>
2022-05-21 14:39:18 -0300

    Conf Colors Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,20 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}"
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        'carrefour': ['Ubuntu', 'arial', 'helvetica']
-      }
+        carrefour: ["Ubuntu", "arial", "helvetica"],
+      },
+      colors: {
+        cfblue: {
+          500: "#1e5bc6",
+          900: "#02296D",
+        },
+        cfred: {
+          500: "#e81e26",
+        },
+      },
     },
   },
   plugins: [],
-}
+};
